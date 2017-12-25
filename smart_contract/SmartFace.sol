43f7e83b7b1e645d3faea6ec2563029bf167f941
VyacheslavLukin <v.s.lukin@gmail.com>
2017-12-25 14:53:16 +0300

    fix typos

--- smart_contract/SmartFace.sol
+++ smart_contract/SmartFace.sol
@@ -5,19 +5,19 @@ pragma solidity ^0.4.16;
 contract SmartFace {
 
     mapping (address => string[]) mapAddressUserHash;
-    mapping (string =>travelStruct[])  mapHachPasport;
+    mapping (string =>travelStruct[])  mapHashPassport;
 
 
-    // to,from,aim,description,timshtamp,hashPasport,address
+    // to,from,aim,description,timestamp,hashPassport,address
 
     struct travelStruct {
-        uint timshtamp;
+        uint timestamp;
         address faceid;
         string to;
         string from;
         string aim;
         string description;
-        string hashPasport;
+        string hashPassport;
     }
 
     function getHash(address _faceId) constant returns (bytes32[]){
@@ -29,21 +29,21 @@ contract SmartFace {
         return result;
     }
 
-    function createUser(address _faceId, string _hashPasport)returns (bool){
-        uint lenght = mapAddressUserHash[_faceId].length;
-        mapAddressUserHash[_faceId].push(_hashPasport);
+    function createUser(address _faceId, string _hashPassport)returns (bool){
+        uint length = mapAddressUserHash[_faceId].length;
+        mapAddressUserHash[_faceId].push(_hashPassport);
         if(mapAddressUserHash[_faceId].length - 1 == lenght)return true;
         return false;
     }
 
-    function getTravelbyHash(string _hashPasport) constant returns(travelStruct[]) {
-        return mapHachPasport[_hashPasport];
+    function getTravelbyHash(string _hashPassport) constant returns(travelStruct[]) {
+        return mapHashPassport[_hashPassport];
 
     }
-    function createTravel(uint _timeshtamp,address _faceId,string _to,string _from,string _aim,
+    function createTravel(uint _timestamp,address _faceId,string _to,string _from,string _aim,
         string _description, string _hashPasport) returns(bool){
-        mapHachPasport[_hashPasport].push(travelStruct(_timeshtamp,_faceId,_to,
-            _from,_aim,_description,_hashPasport));
+        mapHashPassport[_hashPassport].push(travelStruct(_timestamp,_faceId,_to,
+            _from,_aim,_description,_hashPassport));
         return true;
     }
 
